7405a1ceb6f6e7a8918a50918eb364914bca0137
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-08-12 17:30:54 +0700

    modify modifier

--- actions/ImageManagerJsonAction.php
+++ actions/ImageManagerJsonAction.php
@@ -16,7 +16,7 @@ use yii\helpers\FileHelper;
  * @author Nghia Nguyen <yiidevelop@hotmail.com>
  * @since 2.0
  * @modified date 21 May 2018, 12:31 WIB
- * @modified by Putra Sudaryanto <putra@sudaryanto.id>
+ * @modified by Putra Sudaryanto <putra@ommu.co>
  * @contact (+62)856-299-4114
  * @link https://github.com/ommu/yii2-redactor
  */
